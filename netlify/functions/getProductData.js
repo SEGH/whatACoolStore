6965732d94121ef994dfd47f74beb0286424bbea
Susan Holland <segh@fastmail.com>
2021-04-12 18:20:43 -0400

    Testing

--- netlify/functions/getProductData.js
+++ netlify/functions/getProductData.js
@@ -3,7 +3,10 @@ const fetch = require('node-fetch');
 const API_ENDPOINT = `https://b57t0bjz.apicdn.sanity.io/v2021-03-25/data/query/production?query=*[_id=='53b27b6c-e1ee-44fc-bcc6-51e689354ebc']{'id':_id,price,url}&`;
 
 exports.handler = async (event, context) => {
-    console.log("hello world")
+    return {
+        statusCode: 200,
+        body: "Hello, World"
+    };
     // return fetch(API_ENDPOINT, { headers: {Accept: "application/json"}})
     //     .then((response) => response.json())
     //     .then((data) => {
