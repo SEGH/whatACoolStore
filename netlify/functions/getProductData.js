14913d773f5bb795b851d9fd0f38e3865e1b632d
Susan Holland <segh@fastmail.com>
2021-04-12 17:54:53 -0400

    Add serverless function

---
+++ netlify/functions/getProductData.js
@@ -0,0 +1,13 @@
+const fetch = require("note-fetch");
+
+const API_ENDPOINT = `https://b57t0bjz.apicdn.sanity.io/v2021-03-25/data/query/production?query=*[_id=='53b27b6c-e1ee-44fc-bcc6-51e689354ebc']{'id':_id,price,url}&`;
+
+exports.handler = async (event, context) => {
+    return fetch(API_ENDPOINT, { headers: {Accept: "application/json"}})
+        .then((response) => response.json())
+        .then((data) => ({
+            statusCode: 200,
+            body: data.request
+        }))
+        .catch((error) => ({ statusCode: 422, body: String(error)}))
+}
