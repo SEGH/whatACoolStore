69c808a0ea0587b601b96707b655fc180ec1d9f2
Susan Holland <segh@fastmail.com>
2021-04-12 20:00:14 -0400

    Test async

--- netlify/functions/getProductData.js
+++ netlify/functions/getProductData.js
@@ -2,8 +2,8 @@ const fetch = require('node-fetch');
 
 const API_ENDPOINT = `https://b57t0bjz.apicdn.sanity.io/v2021-03-25/data/query/production?query=*[_id=='53b27b6c-e1ee-44fc-bcc6-51e689354ebc']{'id':_id,price,url}&`;
 
-exports.handler = (event, context) => {
-    fetch(API_ENDPOINT, { headers: { Accept: "application/json" } })
+exports.handler = async (event, context) => {
+    return fetch(API_ENDPOINT, { headers: { Accept: "application/json" } })
         .then((response) => response.json())
         .then((data) => {
             console.log(data)
