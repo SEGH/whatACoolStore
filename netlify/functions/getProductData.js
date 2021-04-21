6f0d485d27108da85a7e999f3749f40f2d0d2a7d
Susan Holland <segh@fastmail.com>
2021-04-21 13:23:25 -0400

    Test product array

--- netlify/functions/getProductData.js
+++ netlify/functions/getProductData.js
@@ -1,17 +1,13 @@
 const fetch = require('node-fetch');
 
-const API_ENDPOINT = `https://b57t0bjz.apicdn.sanity.io/v2021-03-25/data/query/production?query=*[_id=='53b27b6c-e1ee-44fc-bcc6-51e689354ebc']{'id':_id,price,url}&`;
+const API_ENDPOINT = `https://b57t0bjz.apicdn.sanity.io/v2021-03-25/data/query/production?query=*[_type=='product']{'id':_id,price,url}&`;
 
 exports.handler = async (event, context) => {
     return fetch(API_ENDPOINT, { headers: { Accept: "application/json" } })
         .then(response => response.json())
         .then(data => ({
             statusCode: 200,
-            body: JSON.stringify({ 
-                id: data.result[0].id,
-                price: "money",
-                url: "/"
-            })
+            body: data.result.forEach(product => JSON.stringify({ id: product.id, price: product.price, url: API_ENDPOINT }))
         }))
 
 }
