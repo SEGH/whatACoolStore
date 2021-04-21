fb122bb32b59f288ecff448164b3f05e70692462
Susan Holland <segh@fastmail.com>
2021-04-20 21:31:12 -0400

    Map sanity products

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import sanityClient from './client';
-// import Product from './components/Product/product';
+import Product from './components/Product/product';
 
 function App() {
   const [products, setProducts] = useState(null);
@@ -17,19 +17,17 @@ function App() {
   return (
     <div>
       <h1>My Cool Store</h1>
-      {/* {products && products.map(product => {
-        return <Product title={product.title} id={product._id} price={product.variants[0].price} />
-      })} */}
-      {/* {products && <Product title={products[0].title} id={products[0]._id} price={products[0].variants[0].price} />}
-    </div> */}
-    <button className="snipcart-add-item"
+      {products && products.map(product => {
+        return <Product key={product._id} title={product.title} id={product._id} price={product.price} url={product.url} />
+      })}
+    {/* <button className="snipcart-add-item"
       data-item-id="1"
       data-item-name="Surprise"
       data-item-price="100.00"
       data-item-url="https://what-a-cool-store.netlify.app"
     >
       BUY
-    </button>
+    </button> */}
 
   </div>
   );
