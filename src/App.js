df534a5407a71467ee6da30b8cee81ab7b4df1f1
Susan Holland <segh@fastmail.com>
2021-04-24 13:09:15 -0400

    Import Header

--- src/App.js
+++ src/App.js
@@ -3,11 +3,13 @@ import './App.css';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import Storefront from './pages/Storefront/Storefront';
 import ProductDetail from './components/ProductDetail/ProductDetail';
+import Header from './components/Header/Header';
 
 function App() {
 
   return (
     <Router>
+      <Header />
       <Route exact path="/" component={Storefront} />
       <Route path="/:slug" component={ProductDetail} />
     </Router>
