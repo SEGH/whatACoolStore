3eb9257bb61c43c2aab2afac980fc71fe7c27685
Susan Holland <segh@fastmail.com>
2021-04-24 12:07:04 -0400

    Add route to Storefront

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import './App.css';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 import Storefront from './pages/Storefront/Storefront';
 
 function App() {
 
   return (
-    <Storefront />
+    <Router>
+      <Route exact path="/" component={Storefront} />
+    </Router>
   );
 }
 
