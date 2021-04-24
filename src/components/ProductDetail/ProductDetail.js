0fb89eeb1e669f4d0562aaf5e63e6684f14de8ae
Susan Holland <segh@fastmail.com>
2021-04-24 12:24:08 -0400

    Fetch productData from sanity with slug

--- src/components/ProductDetail/ProductDetail.js
+++ src/components/ProductDetail/ProductDetail.js
@@ -1,15 +1,30 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { useParams, Link } from 'react-router-dom';
+import sanityClient from '../../client';
 
 
 export default function ProductDetail() {
+    const [productData, setProductData] = useState(null);
     const { slug } = useParams();
+
+    useEffect(() => {
+        sanityClient.fetch(
+            `*[slug.current == $slug]{...}`, { slug }
+        )
+        .then(data => {
+            setProductData(data[0])
+        })
+        .catch(console.error)
+    }, [slug]);
+
+    if (!productData) return (<main id="productDetail">Loading...</main>)
+    
     return (
         <main id="productDetail">
             <section>
                 <Link to={"/"}>All Products</Link>
             </section>
-            {slug}
+            {productData.title}
         </main>
     )
 }
