32799f79e62d2784e6bb8c218f63ab25077aa4b1
Susan Holland <segh@fastmail.com>
2021-04-24 12:13:31 -0400

    Add route to slug

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from 'react';
 import './App.css';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import Storefront from './pages/Storefront/Storefront';
+import ProductDetail from './components/ProductDetail/ProductDetail';
 
 function App() {
 
   return (
     <Router>
       <Route exact path="/" component={Storefront} />
+      <Route path="/:slug" component={ProductDetail} />
     </Router>
   );
 }
