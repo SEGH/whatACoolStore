d576980674c07f141d8e5ff46025481ad00607f5
Susan Holland <segh@fastmail.com>
2021-04-24 12:13:17 -0400

    Add useParams to get slug and Link to route back to home

--- src/components/ProductDetail/ProductDetail.js
+++ src/components/ProductDetail/ProductDetail.js
@@ -1,10 +1,15 @@
 import React from 'react';
+import { useParams, Link } from 'react-router-dom';
 
 
 export default function ProductDetail() {
+    const { slug } = useParams();
     return (
         <main id="productDetail">
-
+            <section>
+                <Link to={"/"}>All Products</Link>
+            </section>
+            {slug}
         </main>
     )
 }
