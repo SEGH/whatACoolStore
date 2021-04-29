1a2546634fd0a26be87811365e8d1a9d630a357e
Susan Holland <segh@fastmail.com>
2021-04-29 18:37:09 -0400

    Add div wrapper around variants, price, and button

--- src/components/ProductSummary/ProductSummary.js
+++ src/components/ProductSummary/ProductSummary.js
@@ -20,25 +20,28 @@ export default function Product({ title, id, price, slug, variants, variantType,
     }
 
     return (
-        <Card header={<Link to={`/${slug}`} className="productLink"><CardTitle image={selectedVariant && selectedVariant.images[0] ? urlFor(selectedVariant.images[0]) : urlFor(mainImage)}></CardTitle></Link>}>
+        <Card header={<Link to={`/${slug}`} className="productLink"><CardTitle image={selectedVariant && selectedVariant.images[0] ? urlFor(selectedVariant.images[0]).size(300, 300) : urlFor(mainImage).size(300, 300)}></CardTitle></Link>}>
 
             <h5>{title}</h5>
-            <div className={!variants ? "invisible" : variants && variants.length === 0 ? "invisible" : ""}>
-                <label htmlFor="variants">{variantType}:</label>
-                <select className="browser-default" name="variants" onChange={(event) => handleSelect(event)}>
-                    {variants && variants.length > 0 && variants.map(variant => <option value={variant.title} key={variant._key}>{variant.title}</option>)}
-                </select>
+            <div className="priceBox">
+                <div className={!variants ? "invisible" : variants && variants.length === 0 ? "invisible" : ""}>
+                    <label htmlFor="variants">{variantType}:</label>
+                    <select className="browser-default" name="variants" onChange={(event) => handleSelect(event)}>
+                        {variants && variants.length > 0 && variants.map(variant => <option value={variant.title} key={variant._key}>{variant.title}</option>)}
+                    </select>
+                </div>
+                <h6>${selectedVariant ? selectedVariant.price : price}</h6>
+                <button className="snipcart-add-item"
+                    data-item-id={id}
+                    data-item-name={title}
+                    data-item-url={`https://what-a-cool-store.netlify.app/.netlify/functions/getProductData`}
+                    data-item-price={price}
+                    data-item-custom1-name={variants && variants.length > 0 ? variantType : null}
+                    data-item-custom1-options={variants && variants.length > 0 ? variantOptions : null}
+                    data-item-custom1-value={selectedVariant && selectedVariant.title}
+                >Add to Cart</button>
             </div>
-            <h6>${selectedVariant ? selectedVariant.price : price}</h6>
-            <button className="snipcart-add-item"
-                data-item-id={id}
-                data-item-name={title}
-                data-item-url={`https://what-a-cool-store.netlify.app/.netlify/functions/getProductData`}
-                data-item-price={price}
-                data-item-custom1-name={variants && variants.length > 0 ? variantType : null}
-                data-item-custom1-options={variants && variants.length > 0 ? variantOptions : null}
-                data-item-custom1-value={selectedVariant && selectedVariant.title}
-            >Add to Cart</button>
+
 
         </Card>
     )
