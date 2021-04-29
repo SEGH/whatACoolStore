a9b21733512b50739acba0a307d4fafa5a785b4f
Susan Holland <segh@fastmail.com>
2021-04-29 19:50:12 -0400

    Import material design icon css

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import 'materialize-css/dist/css/materialize.min.css';
 import 'materialize-css/dist/js/materialize.min.js';
+import 'material-design-icons/iconfont/material-icons.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
